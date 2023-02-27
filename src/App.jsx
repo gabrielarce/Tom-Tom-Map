ba1b79129714673c7dee6a2194f00782014059c2
Gabe <gabrielarce3@gmail.com>
2023-02-26 23:04:18 -0800

    configure Tom Tom API and .ignore files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { useRef, useEffect, useState } from "react";
+import * as tt from "@tomtom-international/web-sdk-maps";
+import TOM_TOM_API_KEY from "../tomTomApiKey";
 
 export default function App() {
-  return <h1>Hola, mundo!</h1>;
+  const mapElement = useRef();
+  const [map, setMap] = useState({});
+
+  useEffect(() => {
+    let map = tt.map({
+      key: TOM_TOM_API_KEY,
+      container: mapElement.current,
+    });
+    setMap(map);
+  }, []);
+  console.log("component loaded");
+  return (
+    <div className="App">
+      <div ref={mapElement}></div>
+    </div>
+  );
 }
